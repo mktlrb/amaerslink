152abee31f8cb26c62e08f5c85278c135b05c352
krazytristan <trstnjorge@gmail.com>
2025-03-23 14:33:10 +0800

    login page of admin

--- generate_hash.php
+++ generate_hash.php
@@ -1,5 +1,5 @@
 <?php
-$password = 'amaerslink2025';  // Original password
+$password = 'admin123';  // Original password
 $hashed_password = password_hash($password, PASSWORD_DEFAULT);  // Hash the password
 echo "Hashed Password: " . $hashed_password;  // Display the hashed password
 ?>
